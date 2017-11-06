faa6fe3430211e50e72f235d86e0acf0ed5a67e1
pvsfair <pvsfair@gmail.com>
2017-11-06 00:53:41 -0300

    Tratamento de Arrays completo

--- LabDeSistemasDistribuidos2017/main.cpp
+++ LabDeSistemasDistribuidos2017/main.cpp
@@ -9,17 +9,45 @@
 #define _DEBUG 1
 #endif
 
+#include <vector>
+#include <math.h>
+
+PyObject* sqrArr(PyObject* self, PyObject* args) {
+	int len;
+	std::vector<long> vect;
+	PyObject* tuple;
+	
+
+	PyListObject* returnTuple;
+	if (PyArg_ParseTuple(args, "O!", &PyList_Type, &tuple)){
+		len = PyList_Size(tuple);
+		vect = std::vector<long>(len);
+		while (len--) {
+			vect[len] = PyInt_AsLong(PyList_GetItem(tuple, len));
+			vect[len] = pow(vect[len],2);
+		}
+		returnTuple = (PyListObject*)PyList_New(vect.size());
+		for (int i = 0; i < vect.size(); i++) {
+			PyList_SET_ITEM(returnTuple, i, PyInt_FromLong(vect[i]));
+		}
+		return (PyObject*)returnTuple;
+	}
+	else
+		return 0;
+}
+
 PyObject* some_function(PyObject* self, PyObject* args) {
-	int i;
-	if (PyArg_ParseTuple(args, "i", &i))
-		return PyInt_FromLong(i+1);
+	int i,j;
+	if (PyArg_ParseTuple(args, "ii", &i, &j))
+		return PyInt_FromLong(i+j);
 	else
 		return 0;
 }
 
 PyMethodDef SpamMethods[] =
 {
-	{"add_one", (PyCFunction) some_function, METH_VARARGS, 0},
+	{ "add_one", (PyCFunction)some_function, METH_VARARGS, 0 },
+	{ "square_array", (PyCFunction)sqrArr , METH_VARARGS, 0 },
 	{0,0,0,0}
 };
 
